95fae7f539f571b8cb596a2b9d75cc7aef582698
Hank Wang <drapho@gmail.com>
2015-06-23 20:14:17 +0800

    add more constants

---
+++ tour/constants-iota2/constants-iota2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ByteSize float64
+
+func (b ByteSize) String() string {
+	switch {
+	case b >= YB:
+		return fmt.Sprintf("%.2fYB", b/YB)
+	case b >= ZB:
+		return fmt.Sprintf("%.2fZB", b/ZB)
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%.2fB", b)
+}
+
+const (
+	_           = iota // ignore first value(0) by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func main() {
+	fmt.Println(TB)
+	fmt.Println(ByteSize(1e6))
+	fmt.Println(ByteSize(1e9))
+	fmt.Println(ByteSize(1e12))
+	fmt.Println(ByteSize(1e13))
+}
