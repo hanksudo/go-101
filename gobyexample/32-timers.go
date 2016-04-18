62fbdeb0f99072206f9c4cf3b2828b3ea8be8b96
Hank Wang <drapho@gmail.com>
2016-04-18 18:30:53 +0800

    add timer example

---
+++ gobyexample/32-timers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	timer1 := time.NewTimer(time.Second * 2)
+
+	<-timer1.C
+	fmt.Println("Timer 1 expired")
+
+	timer2 := time.NewTimer(time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("Timer 2 expired")
+	}()
+
+	// stop timer 2
+	stop2 := timer2.Stop()
+	if stop2 {
+		fmt.Println("Timer 2 stopped")
+	}
+}
