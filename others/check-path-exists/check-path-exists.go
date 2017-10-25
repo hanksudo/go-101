ee452dedb71fd2507353394703965ed54ad8860b
hanksudo <drapho@gmail.com>
2017-10-25 23:15:42 +0800

    update check-path-exists

--- others/check-path-exists/check-path-exists.go
+++ others/check-path-exists/check-path-exists.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 	// "runtime"
 )
 
-func main() {
-	// _, path, _, _ := runtime.Caller(0)
-	path := "not-exist.go"
+func pathExists(path string) {
+	fmt.Printf("Checking %s exists ... \n", path)
 	if info, err := os.Stat(path); os.IsNotExist(err) {
 		fmt.Printf("%s does not exists\n", path)
 	} else {
@@ -17,6 +17,11 @@ func main() {
 		fmt.Println(info.Mode())
 		fmt.Println(info.ModTime())
 		fmt.Println(info.IsDir())
-		// fmt.Println(info.Sys())
+		fmt.Println(info.Sys())
 	}
+}
+func main() {
+	_, path, _, _ := runtime.Caller(0)
+	pathExists(path)
+	pathExists("not-exist.go")
 }
