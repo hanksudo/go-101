4c909e7ea7cbf2cb8e2d2f1acf6eaf0d18f095c6
Hank Wang <drapho@gmail.com>
2018-04-23 21:45:21 +0900

    rename tour files with orders

--- tour/28-range.go
+++ tour/28-range.go
@@ -4,19 +4,18 @@ import (
 	"fmt"
 )
 
-var arr = []int{1, 2, 4, 8, 16, 32, 64, 128}
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
-	for i, v := range arr {
+	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
 	}
 
+	// skip index or value by assigning to _.
 	pow := make([]int, 10)
 	for i := range pow {
-		pow[i] = 1 << uint(i)
+		pow[i] = 1 << uint(i) // == 2**i
 	}
-
-	// discard certain return values
 	for _, value := range pow {
 		fmt.Printf("%d\n", value)
 	}
