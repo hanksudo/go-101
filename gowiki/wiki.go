53d5e80f1f220591e32a24f38f514ebcf2cb8678
Hank Wang <drapho@gmail.com>
2018-04-25 00:52:21 +0900

    [gowiki] create new page if not exists

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -34,7 +34,11 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
