54b3c48f94a4d4222cebbd014e9fd8f246f3d6fb
Hank Wang <drapho@gmail.com>
2018-04-25 00:42:00 +0900

    [gowiki] add edit page

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -36,6 +36,20 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
 	// Test save/load wiki page
 	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page.")}
@@ -44,5 +58,7 @@ func main() {
 	// fmt.Println(string(p2.Body))
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
