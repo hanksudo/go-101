19d1c291503b2b4389fc98dc97c9cf7de464edd0
Hank Wang <drapho@gmail.com>
2019-09-10 23:25:46 +0800

    add language tag matcher

---
+++ others/language-tag-matcher.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/language/display"
+)
+
+var userPrefs = []language.Tag{
+	language.Make("gsw"), // Swiss German
+	language.Make("fr"),  // French
+}
+
+var serverLangs = []language.Tag{
+	language.AmericanEnglish, // en-US fallback
+	language.TraditionalChinese,
+}
+
+var matcher = language.NewMatcher(serverLangs)
+
+func main() {
+	tag, index, confidence := matcher.Match(userPrefs...)
+
+	fmt.Printf("best match: %s (%s) index=%d confidence=%v\n",
+		display.English.Tags().Name(tag),
+		display.Self.Name(tag),
+		index, confidence)
+	// best match: German (Deutsch) index=1 confidence=High
+
+	fmt.Println(matcher.Match(language.Make("zh-Hant")))
+	fmt.Println(matcher.Match(language.Make("zh-tw")))
+	fmt.Println(matcher.Match(language.Make("zh_TW")))
+	fmt.Println(matcher.Match(language.Make("zh-Hant_JP")))
+	fmt.Println(matcher.Match(language.Make("en_US")))
+
+}
