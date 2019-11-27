3c2a5c30f5f4d47c45adae0375b3eb6140685a82
Hank Wang <drapho@gmail.com>
2019-11-27 22:29:11 +0800

    refactor: hello_test.go

--- learn-go-with-tests/hello.go
+++ learn-go-with-tests/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- learn-go-with-tests/hello_test.go
+++ learn-go-with-tests/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("World!")
-	want := "Hello, World!"
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("saying hellp to people", func(t *testing.T) {
+		got := Hello("Hank")
+		want := "Hello, Hank"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
