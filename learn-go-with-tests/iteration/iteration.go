d3984722369efc5b9cabfe49604495b85bb48356
Hank Wang <drapho@gmail.com>
2019-11-27 23:10:48 +0800

    add comment

--- learn-go-with-tests/iteration/iteration.go
+++ learn-go-with-tests/iteration/iteration.go
@@ -1,5 +1,6 @@
 package iteration
 
+// Repeat returns {character} repeated {repeatCount} times
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
