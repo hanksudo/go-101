7345cc62fbcdcd2bca345861fde21a50004149ab
Hank Wang <drapho@gmail.com>
2019-12-16 01:13:59 +0900

    [learn-go-with-tests] update DI

--- learn-go-with-tests/di/di.go
+++ learn-go-with-tests/di/di.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
+// Greet sends a personalised greeting to writer
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+// MyGreeterHandler says Hello, world over HTTP
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	err := http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
