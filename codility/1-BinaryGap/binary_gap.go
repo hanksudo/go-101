4156b345e0af615b728f5d1febcb5d80007e421a
Hank Wang <drapho@gmail.com>
2022-07-08 21:57:36 +0900

    feat: add more solution on codility

---
+++ codility/1-BinaryGap/binary_gap.go
@@ -0,0 +1,26 @@
+package binarygap
+
+func Solution(N int) int {
+	ans := 0
+	last := -1
+	pos := 0
+	for {
+		if N == 0 {
+			return ans
+		}
+		m := N % 2
+		if m == 1 {
+			if last == -1 {
+				last = pos
+			} else {
+				diff := pos - last - 1
+				if diff > ans {
+					ans = diff
+				}
+				last = pos
+			}
+		}
+		N /= 2
+		pos += 1
+	}
+}
---
+++ codility/1-BinaryGap/binary_gap_test.go
@@ -0,0 +1,34 @@
+package binarygap
+
+import (
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	t.Run("9", func(t *testing.T) {
+		got := Solution(9)
+		want := 2
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, 9)
+		}
+	})
+
+	t.Run("529", func(t *testing.T) {
+		got := Solution(529)
+		want := 4
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, 529)
+		}
+	})
+
+	t.Run("20", func(t *testing.T) {
+		got := Solution(20)
+		want := 1
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, 20)
+		}
+	})
+}
