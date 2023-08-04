2a4a4ff0c4364d7c0221eec309835cb9cd2c190b
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-08-04 15:14:55 +0900

    set corret order of file

--- gobyexample/60-sha256-hashes.go
+++ gobyexample/60-sha256-hashes.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"crypto/sha1"
+	"crypto/sha256"
 	"fmt"
 )
 
 func main() {
-	s := "sha1 this string"
-	h := sha1.New()
+	s := "sha256 this string"
+	h := sha256.New()
 	h.Write([]byte(s))
 	bs := h.Sum(nil)
 	fmt.Println(s)
